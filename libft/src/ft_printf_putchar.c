8a889c6c5688544e71d157cec307540d3ff33092
Nadège Moisan <nadege@MacBook-Pro-de-Nadege.local>
2024-07-18 13:46:50 -0400

    modifier printf par ft_printf car printf leak

--- libft/src/ft_printf_putchar.c
+++ libft/src/ft_printf_putchar.c
@@ -3,19 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf_putchar.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: namoisan <namoisan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nadege <nadege@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 16:24:25 by namoisan          #+#    #+#             */
-/*   Updated: 2023/12/19 11:09:40 by namoisan         ###   ########.fr       */
+/*   Updated: 2024/07/18 13:40:31 by nadege           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
-
-void	ft_putchar_fd(char c, int fd)
-{
-	write(fd, &c, 1);
-}
 
 int	ft_putchar(char c)
 {
--- libft/src/ft_printf_putstr.c
+++ libft/src/ft_printf_putstr.c
@@ -3,26 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf_putstr.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: namoisan <namoisan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nadege <nadege@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 16:38:40 by namoisan          #+#    #+#             */
-/*   Updated: 2023/12/19 12:02:41 by namoisan         ###   ########.fr       */
+/*   Updated: 2024/07/18 13:40:16 by nadege           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
-
-void	ft_putstr_fd(char *s, int fd)
-{
-	int	i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		write(fd, &s[i], 1);
-		i++;
-	}
-}
 
 int	ft_putstr(char *str)
 {
