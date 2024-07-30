4b422c7fbcda2cbc4f62b64b839a411900b1734a
Vincent Boulanger <vboulang@c1r4p9.42quebec.com>
2024-07-30 10:16:32 -0400

    add checks to redir output

--- src/execution/redirections.c
+++ src/execution/redirections.c
@@ -2,8 +2,16 @@
 
 static int	set_l_redir(t_data *mini, t_redir	*redir, t_command *cmd)
 {
-	if (access(redir->file_name, R_OK) == 0)
+	if (access(redir->file_name, F_OK) == 0)
 	{
+		if (access(redir->file_name, R_OK) != 0)
+		{
+			ft_putstr_fd("minishell: ", 2);
+			ft_putstr_fd(redir->file_name, 2);
+			ft_putstr_fd(": Permission denied\n", 2);
+			mini->exit_status = 1;
+			return (1);
+		}
 		cmd->fdin = to_open(redir);
 		if (cmd->fdin == -1)
 		{
@@ -18,23 +26,34 @@ static int	set_l_redir(t_data *mini, t_redir	*redir, t_command *cmd)
 		ft_putstr_fd("minishell: ", 2);
 		ft_putstr_fd(redir->file_name, 2);
 		ft_putstr_fd(": No such file or directory\n", 2);
-		mini->exit_status = 126;
+		mini->exit_status = 1;
 		return (1);
 	}
 }
 
-static void	set_r_redir(t_redir	*redir, t_command *cmd)
+static int	set_r_redir(t_data *mini, t_redir	*redir, t_command *cmd)
 {
 	if (redir->type == R1_REDIR)
 	{
 		cmd->fdout = to_open(redir);
+		if (cmd->fdin == -1)
+		{
+			mini->exit_status = 1;
+			return (1);
+		}
 		change_output(cmd->fdout);
 	}
 	else if (redir->type == R2_REDIR)
 	{
 		cmd->fdout = to_open(redir);
+		if (cmd->fdin == -1)
+		{
+			mini->exit_status = 1;
+			return (1);
+		}
 		change_output(cmd->fdout);
 	}
+	return (0);
 }
 
 int	set_redir(t_data *mini, int pnb)
@@ -56,7 +75,10 @@ int	set_redir(t_data *mini, int pnb)
 				return (1);
 		}
 		else if (redir->type == R1_REDIR || redir->type == R2_REDIR)
-			set_r_redir(redir, cmd);
+		{
+			if (set_r_redir(mini, redir, cmd) == 1)
+				return (1);
+		}
 		redir = redir->next;
 	}
 	return (0);
